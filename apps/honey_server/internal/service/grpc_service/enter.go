47e00fdf423ff67fa9ca60bc47f837eb16b27e3b
05allan1213 <2787554820@qq.com>
2025-11-16 15:28:25 +0800

    grpc加密配置

--- apps/honey_server/internal/service/grpc_service/enter.go
+++ apps/honey_server/internal/service/grpc_service/enter.go
@@ -4,12 +4,16 @@ package grpc_service
 // Description: gRPC服务，负责启动gRPC服务器，注册节点服务处理器，处理节点注册等gRPC请求。
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"honey_server/internal/global"
 	"honey_server/internal/rpc/node_rpc"
+	"io/ioutil"
 	"net"
 
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 // NodeService 节点服务的gRPC处理器结构体
@@ -20,7 +24,7 @@ type NodeService struct {
 
 // Run 启动gRPC服务的入口函数
 //
-// 1. 监听配置文件中指定的gRPC端口
+// 1. 监听配置文件中指定的gRPC端口,并加载服务端证书和私钥
 // 2. 创建gRPC服务器实例
 // 3. 注册节点服务处理器
 // 4. 启动服务器并开始处理请求
@@ -33,9 +37,34 @@ func Run() {
 		logrus.Fatalf("Failed to listen: %v", err)
 	}
 
-	// 创建一个新的gRPC服务器实例
-	s := grpc.NewServer()
-	// 实例化节点服务处理器
+	// 加载服务端证书和私钥
+	cert, err := tls.LoadX509KeyPair("cert/server.crt", "cert/server.key")
+	if err != nil {
+		logrus.Fatalf("failed to load key pair: %v", err)
+	}
+
+	// 加载 CA 证书
+	caCert, err := ioutil.ReadFile("cert/ca.crt")
+	if err != nil {
+		logrus.Fatalf("failed to read CA certificate: %v", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// 创建 TLS 配置
+	config := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert, // 双向认证
+		ClientCAs:    caCertPool,
+	}
+
+	// 创建 credentials
+	creds := credentials.NewTLS(config)
+
+	// 创建 gRPC 服务器，使用 TLS credentials
+	s := grpc.NewServer(grpc.Creds(creds))
+
+	// 创建一个gRPC节点服务器实例。
 	server := NodeService{}
 	// 将节点服务处理器注册到gRPC服务器，使其能处理对应的gRPC请求
 	node_rpc.RegisterNodeServiceServer(s, &server)
